43409c5523f638ae8417c870ece1ef41496f52c1
Sean McLin <srmclin93@yahoo.com>
2022-11-21 15:17:36 -0800

    commit on master

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { ConnectExtension } from "@magic-ext/connect";
 import Web3 from "web3";
 import './App.css'
 
-const magic = new Magic(import.meta.env.MAGIC_API_KEY, {
+const magic = new Magic(import.meta.VITE_MAGIC_KEY, {
   network: "goerli",
   locale: "en_US",
   extensions: [new ConnectExtension()]
