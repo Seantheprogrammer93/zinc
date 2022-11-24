5e06a436ec0a30b5ddcde8576ac66d1ca560357e
Sean McLin <srmclin93@yahoo.com>
2022-11-24 12:53:38 -0800

    commit on master

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,35 +3,39 @@ import React from 'react'
 function Header(props) {
   return (
     <>
-    <header aria-label="Page Header">
-      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:px-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <div className="text-center sm:text-left">
-            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
-              Welcome!
-            </h1>
-          </div>
+      <header aria-label="Page Header">
+        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:px-8">
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <div className="text-center sm:text-left">
+              <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+                Zinc
+              </h1>
+            </div>
 
-          <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
-            <button
-              className="inline-flex items-center justify-center rounded-lg border border-gray-200 px-5 py-3 text-gray-500 transition hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:ring"
-              type="button"
-              onClick={props.showWalletButton}
-            >
-              Show Wallet 💵
-            </button>
-            
-            <button
-              className="block rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring"
-              type="button"
-              onClick={props.logoutButton}
-            >
-              Logout
-            </button>
+            <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
+              <button
+                className="inline-flex items-center justify-center rounded-lg border border-gray-200 px-5 py-3 text-gray-500 transition hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:ring"
+                type="button"
+                onClick={props.showWalletButton}
+              >
+                <span class="mr-2">Wallet/Account</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                </svg>
+              </button>
+
+              <button
+                className="block rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring"
+                type="button"
+                onClick={props.logoutButton}
+              >
+                Logout
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
     </>
   )
 }
