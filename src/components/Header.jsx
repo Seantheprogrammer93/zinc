4b88e1a1d618ba1118b55f3af3504d39f56e79e1
Sean McLin <srmclin93@yahoo.com>
2022-11-24 07:45:20 -0800

    Commit on master

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,14 +16,6 @@ function Header(props) {
             <button
               className="inline-flex items-center justify-center rounded-lg border border-gray-200 px-5 py-3 text-gray-500 transition hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:ring"
               type="button"
-              onClick={""}
-            >
-              Send Transaction ➡️
-            </button>
-
-            <button
-              className="inline-flex items-center justify-center rounded-lg border border-gray-200 px-5 py-3 text-gray-500 transition hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:ring"
-              type="button"
               onClick={props.showWalletButton}
             >
               Show Wallet 💵
