7ec49134f39142ef1efc73748b45dc8594b93e54
Sean McLin <srmclin93@yahoo.com>
2022-11-24 10:31:44 -0800

    commit on master

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
     const txnParams = {
       from: publicAddress,
       to: toAddress,
-      value: web3.utils.toWei(parseFloat(amount), "ether"),
+      value: web3.utils.toWei(amount, "ether"),
       gasPrice: web3.utils.toWei("30", "gwei")
     };
     web3.eth
